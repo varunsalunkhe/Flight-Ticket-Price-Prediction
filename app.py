224c5f263330725e5ad170787b85c6a8eebc8f4f
varun <varunsalunkhe07@gmail.com>
2023-01-13 22:48:25 +0530

    new app.py file added

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from flask import Flask, render_template, request, url_for, redirect, session,jsonify
-import pymongo
-import bcrypt
+# import pymongo
+# import bcrypt
 import pandas as pd
 import numpy as np
 import joblib
+import traceback
 # from logger.logger import log
 
 
 model= joblib.load("model.pkl")
+print("model added.")
 # log.addLog("INFO","model loaded")
 
 first_half_columns=joblib.load("first_half_columns.pkl")
+print("first added.")
 # log.addLog("INFO","columns loaded")
 
 sec_half_columns= joblib.load("sec_half_columns.pkl")
+print("sec added.")
 # log.addLog("INFO","columns loaded")
 
 all_cols= joblib.load("all_cols.pkl")
+print("model added.")
 # log.addLog("INFO","columns loaded")
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 # def dayy(date):
 #     dat=[]
@@ -41,23 +43,15 @@ def index():
 #     t.append(d)
 #     return t
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
 @app.route("/dashboard" , methods=["GET","POST"])
 
-def dayy(date):
-    dat=[]
-    a= pd.to_datetime(date, format= "%d-%m-%Y").day
-    b= pd.to_datetime(date, format= "%d-%m-%Y").month
-    dat.append(a)
-    dat.append(b)
-    return dat
 
-def timer(tt):
-    t=[]
-    c= pd.to_datetime(tt).hour
-    d= pd.to_datetime(tt).minute
-    t.append(c)
-    t.append(d)
-    return t
 
 
 def predict():
@@ -65,10 +59,11 @@ def predict():
         try:
         # email = session["email"]
         # data= [str(i) for i in request.form.values()]
-        
+            print("model added.")
+
             tess2 = request.json
             f_half = tess2[:4]
-            new_input1 = pd.DataFrame(np.array(f_half).reshape(1,-1),columns= first_half_columns)
+            new_input1 = pd.DataFrame(np.array(f_half).reshape(1,-1), columns= first_half_columns)
             new_input1= pd.get_dummies(new_input1)
             final1=new_input1.reindex(columns=all_cols , fill_value=0)
 
@@ -87,9 +82,10 @@ def predict():
             final= pd.concat([final1,new_input2], axis = 1)
 
             prediction = model.predict(final)
-            return jsonify({"Prediction ": prediction})
+            return jsonify({"Prediction ": str(prediction)})
 
         except:
+            print("fu")
             return jsonify({"trace ": traceback.format_exc()})
 
             
@@ -102,7 +98,23 @@ def predict():
         # return render_template("login.html")
         return ("no model is here to use")
 
+def dayy(date):
+    dat=[]
+    a= pd.to_datetime(date, format= "%d-%m-%Y").day
+    b= pd.to_datetime(date, format= "%d-%m-%Y").month
+    dat.append(a)
+    dat.append(b)
+    return dat
+
+def timer(tt):
+    t=[]
+    c= pd.to_datetime(tt).hour
+    d= pd.to_datetime(tt).minute
+    t.append(c)
+    t.append(d)
+    return t
+
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
